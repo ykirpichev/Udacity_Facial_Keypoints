2a1d254dcdb88b069a94da9d3ea5de397d3f5413
Yury Kirpichev <ykirpichev@gmail.com>
2018-05-09 00:35:57 +0300

    Implement models

--- models.py
+++ models.py
@@ -7,7 +7,65 @@ import torch.nn.functional as F
 # can use the below import should you choose to initialize the weights of your Net
 import torch.nn.init as I
 
+class Lenet5(nn.Module):
+    """
+    Input - 1x32x32
+    C1 - 6@28x28 (5x5 kernel)
+    tanh
+    S2 - 6@14x14 (2x2 kernel, stride 2) Subsampling
+    C3 - 16@10x10 (5x5 kernel, complicated shit)
+    tanh
+    S4 - 16@5x5 (2x2 kernel, stride 2) Subsampling
+    C5 - 120@1x1 (5x5 kernel)
+    F6 - 84
+    tanh
+    F7 - 10 (Output)
+    """
+    def __init__(self):
+        super(Lenet5, self).__init__()
+        
+        ## TODO: Define all the layers of this CNN, the only requirements are:
+        ## 1. This network takes in a square (same width and height), grayscale image as input
+        ## 2. It ends with a linear layer that represents the keypoints
+        ## it's suggested that you make this last layer output 136 values, 2 for each of the 68 keypoint (x, y) pairs
+        
+        # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
+        # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
+        self.conv1 = nn.Conv2d(1, 10, 3)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(10, 32, 3)
+        self.conv3 = nn.Conv2d(32, 64, 3)
+        
+        self.fc1 = nn.Linear(120, 84)
+        self.fc2 = nn.Linear(84, 10)
+        
+        self.dropout = nn.Dropout(p=0.4)
+        
+        ## Note that among the layers to add, consider including:
+        # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
+        
 
+        
+    def forward(self, x):
+        ## TODO: Define the feedforward behavior of this model
+        ## x is the input image and, as an example, here you may choose to include a pool/conv step:
+        ## x = self.pool(F.relu(self.conv1(x)))
+        
+        output = self.pool(F.relu(self.conv1(x)))
+        output = output.view(-1, 120)
+        output = self.fc1(output)
+
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(x.size(0), -1)
+        x = self.dropout(F.relu(self.fc1(x)))
+        x = self.softmax(self.fc2(x))
+        # final output
+        return x
+        # a modified x, having gone through all the layers of your model, should be returned
+        return output   
+
+# https://arxiv.org/pdf/1710.00977.pdf
 class Net(nn.Module):
 
     def __init__(self):
@@ -20,8 +78,18 @@ class Net(nn.Module):
         
         # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
         # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
-        self.conv1 = nn.Conv2d(1, 32, 5)
+        self.conv1 = nn.Conv2d(1, 32, 4)
+        self.conv2 = nn.Conv2d(32, 64, 3)
+        self.conv3 = nn.Conv2d(64, 128, 2)
+        self.conv4 = nn.Conv2d(128, 256, 1)
         
+        self.pool = nn.MaxPool2d(2, 2)
+        
+        self.dropout1 = nn.Dropout(p=0.2)
+        self.dropout2 = nn.Dropout(p=0.4)
+        
+        self.fc1 = nn.Linear(43264, 1000)
+        self.fc2 = nn.Linear(1000, 136)
         ## Note that among the layers to add, consider including:
         # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
         
@@ -31,7 +99,13 @@ class Net(nn.Module):
         ## TODO: Define the feedforward behavior of this model
         ## x is the input image and, as an example, here you may choose to include a pool/conv step:
         ## x = self.pool(F.relu(self.conv1(x)))
-        
-        
+        output = self.dropout1(self.pool(F.relu(self.conv1(x))))
+        output = self.dropout1(self.pool(F.relu(self.conv2(output))))
+        output = self.dropout1(self.pool(F.relu(self.conv3(output))))
+        output = self.dropout1(self.pool(F.relu(self.conv4(output))))
+        # flatten
+        output = output.view(output.size(0), -1)
+        output = self.dropout2(F.relu(self.fc1(output)))
+        output = self.fc2(output)
         # a modified x, having gone through all the layers of your model, should be returned
-        return x
+        return output
